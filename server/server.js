9332831c4c096a90d78d64de7b474ac514a33449
Gabriel Ribeiro <gribeiro8@gmail.com>
2019-11-18 09:18:52 -0300

    Modification for heroku hosting

--- server/server.js
+++ server/server.js
@@ -13,12 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(allowCors);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "/../client", "build", "index.html"))
-    })
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => { res.sendfile(path.join(__dirname = '../client/build/index.html')); })
 }
 
 
